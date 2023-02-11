ad9d6440a3cc22a2d0570ad1383fa1c0b28cbe7a
Dimitar Iliev <dimitar@MBP-de-Dimitar.technicolor.net>
2023-02-11 11:33:32 +0100

    CW2

--- CodeWars2.py
+++ CodeWars2.py
@@ -1,8 +1,9 @@
 # Task: Given an integral number, determine if it's a square number.
+import math
 
 def is_square(n):
-    
-    
-    
-    
-    return false
+    if n > -1:
+        result = math.sqrt(n).is_integer()
+        return result
+    else: 
+        return False    
